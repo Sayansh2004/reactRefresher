b4a83b2b66cdf712107aa46b0e93abcacdddb9a8
unknown <choprasayansh@gmail.com>
2026-02-10 00:40:17 +0530

    written a cleaner way to fetch data

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -17,12 +17,33 @@ export default function RestaurantMenu(){
 
         setResInfo(json);
     }
-    return resInfo===null?(
-        <Card/>
-    ):(
-        <div>
-        <h1>{resInfo?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants[0]?.info?.name}</h1>
-        </div>
-    );
+
+    if (resInfo === null) {
+    return <Card />;
+  }
+ 
+    const {name,cloudinaryImageId,avgRating,cuisines}=resInfo?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants[0]?.info;
+
+    
+    
+   return (
+   <div className="w-64 bg-white shadow-lg rounded-xl overflow-hidden hover:scale-105 transition-transform duration-200">
+  
+  <img
+    src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId}
+    alt={name}
+    className="w-full h-40 object-cover"
+  />
+
+  <div className="p-4">
+    <h1 className="text-lg font-semibold">{name}</h1>
+    <h3 className="text-sm text-gray-600">
+      {cuisines.join(", ")} • {avgRating} stars
+    </h3>
+  </div>
+
+</div>
+
+  );
       
 }
